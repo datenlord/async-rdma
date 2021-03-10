5995318e1042516cfc866302fe940a6eeca1b7e9
Pu Wang <nicolas.weeks@gmail.com>
2021-03-10 10:48:06 +0800

    soft-roce setup

--- run.sh
+++ run.sh
@@ -1,23 +1,68 @@
 #! /bin/sh
 
+# Remove related kernel modules if any
+sudo rmmod rdma_rxe
+sudo rmmod siw
+sudo modprobe -r --remove-dependencies rdma_ucm
+sudo modprobe -r --remove-dependencies ib_core
+
 set -o errexit
 set -o nounset
 set -o xtrace
 
-# Build and install softiwarp kernel module
-cd ./src/siw
-make
+HOST_IP=`ifconfig eth0 | grep 'inet ' | awk '{print $2}'`
+SRV_PORT=9527
+MSG_CNT=10
+RXE_DEV=rxe_eth0
+
+# Load required IB and RDMA kernel modules
 sudo modprobe ib_core
 sudo modprobe rdma_ucm
+
+# Build and install soft-roce kernel module
+# The rxe code is from Kernel 5.5.0
+cd ./src/rxe
+make
+sudo insmod ./rdma_rxe.ko
+
+# Setup soft-roce device
+sudo rdma link add $RXE_DEV type rxe netdev eth0
+rdma link | grep $RXE_DEV
+
+# Test soft-roce
+ibv_rc_pingpong -d $RXE_DEV -g 0 &
+sleep 1
+ibv_rc_pingpong -d $RXE_DEV -g 0 $HOST_IP
+
+udaddy &
+sleep 1
+udaddy -s $HOST_IP
+
+rping -s -C $MSG_CNT -v &
+sleep 1
+rping -c -a $HOST_IP -C $MSG_CNT -v
+
+rdma_server -s 0.0.0.0 -p $SRV_PORT &
+sleep 1
+rdma_client -s $HOST_IP -p $SRV_PORT
+
+ucmatose &
+sleep 1
+ucmatose -s $HOST_IP
+
+# Remove soft-roce device
+sudo rdma link delete $RXE_DEV
+
+
+# Build and install softiwarp kernel module
+# The siw code is from Kernel 5.4.0
+cd ../siw
+make
 sudo insmod ./siw.ko
 
 # Setup softiwarp device
 sudo rdma link add siw_eth0 type siw netdev eth0
 rdma link | grep siw_eth0
-
-HOST_IP=`ifconfig eth0 | grep 'inet ' | awk '{print $2}'`
-SRV_PORT=9527
-MSG_CNT=10
 
 # Test softiwarp
 rping -s -C $MSG_CNT -v &
@@ -35,3 +80,7 @@ ucmatose -s $HOST_IP
 rdma_xserver -p $SRV_PORT -c r &
 sleep 1
 rdma_xclient -s $HOST_IP -p $SRV_PORT -c r
+
+# Remove softiwarp device
+sudo rdma link delete siw_eth0
+

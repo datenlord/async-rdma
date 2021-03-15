14c644095e96a6eaa03620d8d01a4ea99c731f4f
Pu Wang <nicolas.weeks@gmail.com>
2021-03-15 17:23:40 +0000

    unsafe rust rdma

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,51 @@
+//! RDMA Rust Async API
+
+#![deny(
+    // The following are allowed by default lints according to
+    // https://doc.rust-lang.org/rustc/lints/listing/allowed-by-default.html
+    anonymous_parameters,
+    bare_trait_objects,
+    // box_pointers, // use box pointer to allocate on heap
+    elided_lifetimes_in_paths, // allow anonymous lifetime
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs, // TODO: add documents
+    single_use_lifetimes, // TODO: fix lifetime names only used once
+    trivial_casts, // TODO: remove trivial casts in code
+    trivial_numeric_casts,
+    // unreachable_pub, allow clippy::redundant_pub_crate lint instead
+    // unsafe_code,
+    unstable_features,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    // unused_results, // TODO: fix unused results
+    variant_size_differences,
+
+    warnings, // treat all wanings as errors
+
+    clippy::all,
+    clippy::restriction,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::cargo
+)]
+#![allow(
+    // Some explicitly allowed Clippy lints, must have clear reason to allow
+    clippy::blanket_clippy_restriction_lints, // allow denying clippy::restriction directly
+    clippy::implicit_return, // actually omitting the return keyword is idiomatic Rust code
+    clippy::module_name_repetitions, // repeation of module name in a struct name is not big deal
+    clippy::multiple_crate_versions, // multi-version dependency crates is not able to fix
+    clippy::panic, // allow debug_assert, panic in production code
+)]
+
 use std::env;
 use std::ffi::CString;
 use std::os::raw::{c_char, c_int};
 use std::os::unix::ffi::OsStringExt;
+use utilities::Cast;
+
+mod basic;
 
 #[cfg(target_os = "linux")]
 extern "C" {
@@ -9,16 +53,52 @@ extern "C" {
     fn client_main(argc: c_int, argv: *const *const c_char) -> c_int;
 }
 
-fn main() {
+const SERVER_PORT_ARG_NAME: &str = "server_port";
+
+fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let matches = clap::App::new("DatenLord")
+        .about("Cloud Native Storage")
+        .arg(
+            clap::Arg::with_name(SERVER_PORT_ARG_NAME)
+                .short("p")
+                .long(SERVER_PORT_ARG_NAME)
+                .value_name("SERVER_PORT")
+                .takes_value(true)
+                .required(true)
+                .help(
+                    "Set the RDMA server port, no default value",
+                ),
+        )
+        .get_matches();
+    let server_port = match matches.value_of(SERVER_PORT_ARG_NAME) {
+        Some(sp) => sp.parse::<u16>()?,
+        None => panic!("No server port input"),
+    };
+
+    let mut srv = unsafe { basic::server::Server::new(server_port) };
+    unsafe { srv.run() };
+
     let v: Vec<CString> = env::args_os()
-        .map(|a| CString::new(a.into_vec()).expect("failed to convert os-string to c-string"))
+        .map(|a| {
+            CString::new(a.into_vec()).unwrap_or_else(|err| {
+                panic!(
+                    "failed to convert os-string to c-string, the err is: {}",
+                    err,
+                )
+            })
+        })
         .collect();
     let a: Vec<*const c_char> = v.iter().map(|c| c.as_ptr()).collect();
+    #[allow(clippy::as_conversions)]
     unsafe {
         if a.len() > 2 {
-            client_main(a.len() as c_int, a.as_ptr());
+            client_main(a.len().cast(), a.as_ptr());
         } else {
-            server_main(a.len() as c_int, a.as_ptr());
+            server_main(a.len().cast(), a.as_ptr());
         }
     }
+
+    Ok(())
 }
